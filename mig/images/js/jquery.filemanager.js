c178d10a95402ecfc0aa1e7229c0aad5ad247ecf
simon.a.f.lund <simon.a.f.lund@b75ad72c-e7d7-11dd-a971-7dbc132099af>
2009-10-13 13:48:18 +0000

    Added a bunch of frontend stuff (jQuery, images, css) used in the filemanager/jobmanager json-based gui's. Removed a bunch of unused images.

---
+++ mig/images/js/jquery.filemanager.js
@@ -0,0 +1,162 @@
+if (jQuery) (function($){
+  
+  $.fn.filemanager = function(user_options) {
+
+    var defaults = {
+        root: '/',      
+        connector: 'somewhere.py',
+        folderEvent: 'click',  
+        expandSpeed: 500,
+        collapseSpeed: 500,
+        expandEasing: null,
+  			collapseEasing: null,
+        multiFolder: true,
+        loadMessage: 'Loading...'
+    };
+    var options = $.extend(defaults, user_options);
+
+    return this.each(function() {
+      obj = $(this);
+            
+      function showTree(folder_pane, t) {
+        
+        //var folder_pane = $('.fm_folders', obj);
+        var file_pane   = $('.fm_files', obj);
+        var toolbar     = $('.fm_toolbar', obj);
+        var statusbar   = $('.fm_statusbar', obj);
+        var addressbar  = $('.fm_addressbar', obj);
+        
+        $(folder_pane).addClass('wait');
+        $(".jqueryFileTree.start").remove();
+
+        $.getJSON(options.connector, { dir: t }, function(listing, textStatus) {
+          
+          var i=0;
+          var folders = '<ul class="jqueryFileTree">';          
+          
+          /*
+          var files = '<table>'+
+                      '<thead>'+
+                      ' <tr><td>&nbsp;</td><td>&nbsp;</td><td>Name</td><td>Size</td><td>Date Modified</td></tr>'+
+                      ' </thead>'+
+                      ' <tbody>';*/
+                      
+          var files = '<ul class="jqueryFileList">';
+          
+          var total_file_size = 0;
+          var file_count = 0.0;          
+          var is_dir = false;
+          var base_css_style = 'file';
+          
+          for (i=0;i<listing.length;i++) {
+            
+            is_dir = listing[i]['is_dir'];
+            base_css_style = 'file';
+            if (is_dir) {
+              base_css_style = 'directory';
+              folders += '<li class="'+base_css_style+' collapsed"><a href="#" rel="'+listing[i]['parent']+'/">'+listing[i]['name']+'</a></li>\n';
+            }
+            /*
+            files +=  '<tr>'+
+                      '<td class="bulk"><input type="checkbox" /></td>'+
+                      ' <td class="'+base_css_style+' ext_'+listing[i]['ext']+'">&nbsp;</td>'+
+                      ' <td>'+listing[i]['name']+'</td>'+
+                      ' <td style="text-align: right;">'+pp_bytes(listing[i]['size'])+'</td>'+
+                      ' <td>'+pp_date(listing[i]['create_time'])+'</td>'+
+                      '</tr>';*/
+            files +=  '<li class="'+base_css_style+' ext_'+listing[i]['ext']+'">'+
+                      ' <span class="size">'+pp_bytes(listing[i]['size'])+'</span>'+
+                      ' <span class="cdate">'+pp_date(listing[i]['create_time'])+'</span>'+
+                      ' <span class="bulk"><input type="checkbox" /></span>'+
+                      ' <span class="name">'+listing[i]['name']+'</span>'+
+                      
+                      '</li>';
+
+            total_file_size += listing[i]['size'];
+            file_count++;
+            
+          }
+          folders, files += '</ul>';
+          
+          //files += '</ul>'
+          /*files +=  ' </tbody>'+
+                    '</table>';*/
+                    
+          addressbar.find('input').val(t);
+          folder_pane.removeClass('wait').append(folders);
+          file_pane.removeClass('wait').html(files);
+          statusbar.html(file_count+' files in current folder of total '+pp_bytes(total_file_size)+' bytes in size.');
+  
+          if( options.root == t ) {
+            folder_pane.find('UL:hidden').show();
+          } else {
+            folder_pane.find('UL:hidden').slideDown({ duration: options.expandSpeed, easing: options.expandEasing });
+          }
+          
+          /* Associate context-menus */
+          $(".directory").contextMenu({ menu: 'folder_context'},
+                                            function(action, el, pos) {
+                                            alert(
+                                                'Action: ' + action + ',' +
+                                                'Element ID: ' + $(el).find('a').attr('rel') + ',' + el.find('li') +
+                                                'X: ' + pos.x + '  Y: ' + pos.y + ' (relative to element),' +
+                                                'X: ' + pos.docX + '  Y: ' + pos.docY+ ' (relative to document)'
+                                                );
+                                            });
+          
+          $(".file").contextMenu({ menu: 'file_context'},
+                                            function(action, el, pos) {
+                                            alert(
+                                                'Action: ' + action + ',' +
+                                                'Element ID: ' + $(el).attr('class') + ',' + el.find('li') +
+                                                'X: ' + pos.x + '  Y: ' + pos.y + ' (relative to element),' +
+                                                'X: ' + pos.docX + '  Y: ' + pos.docY+ ' (relative to document)'
+                                                );
+                                            });
+          
+					bindTree(folder_pane);
+        });
+
+      }
+      
+      function bindTree(t) {
+        $(t).find('LI A').bind(options.folderEvent, function() {
+          if( $(this).parent().hasClass('directory') ) {
+            if( $(this).parent().hasClass('collapsed') ) {
+              // Expand
+              if( !options.multiFolder ) {
+                $(this).parent().parent().find('UL').slideUp({ duration: options.collapseSpeed, easing: options.collapseEasing });
+                $(this).parent().parent().find('LI.directory').removeClass('expanded').addClass('collapsed');
+              }
+              $(this).parent().find('UL').remove(); // cleanup
+              showTree( $(this).parent(), escape($(this).attr('rel').match( /.*\// )) );
+              $(this).parent().removeClass('collapsed').addClass('expanded');
+            } else {
+              // Collapse
+              $(this).parent().find('UL').slideUp({ duration: options.collapseSpeed, easing: options.collapseEasing });
+              $(this).parent().removeClass('expanded').addClass('collapsed');
+            }
+          } else {
+            h($(this).attr('rel'));
+          }
+          return false;
+        });
+        // Prevent A from triggering the # on non-click events
+        if( options.folderEvent.toLowerCase != 'click' ) $(t).find('LI A').bind('click', function() { return false; });
+			}
+
+      // Loading message
+      $('.fm_folders', obj).html('<ul class="jqueryFileTree start"><li class="wait">' + options.loadMessage + '<li></ul>');
+      $('.fm_files', obj).html('<ul class="jqueryFileTree start"><li class="wait">' + options.loadMessage + '<li></ul>');
+      
+      // Get the initial file list
+
+      
+      showTree( $('.fm_folders', obj), escape(options.root) );
+      
+
+    });
+
+ };
+ 
+})(jQuery);

c0c4a7b712c1b527eb02c7f014c0e0309c4af5cc
jb.diku <jb.diku@b75ad72c-e7d7-11dd-a971-7dbc132099af>
2010-02-09 15:42:26 +0000

    Merge in changes 846:853 from trunk.

--- branches/grid.dk/mig/images/js/jquery.jobmanager.js
+++ branches/grid.dk/mig/images/js/jquery.jobmanager.js
@@ -277,9 +277,16 @@ if (jQuery) (function($){
         var job_count = 0;
         var sched_hint = '';
         var output_url = '';
-        
+        var max_jobs = -1;
+        var limit_opts = "max_jobs=100";
+
+        // Read out current max jobs setting from selector
+        max_jobs = parseInt($(".maxjobs", config.container).val());
+        if (max_jobs > 0) {
+            limit_opts = "flags=s;max_jobs=" + max_jobs;
+        }
         // add some html      
-        $.getJSON("jobstatus.py?output_format=json", {}, function(jsonRes, textStatus) {
+        $.getJSON("jobstatus.py?output_format=json;"+limit_opts, {}, function(jsonRes, textStatus) {
         
             var jobList = new Array();
             var i = 0;

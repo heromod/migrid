46f3aed85da07419cf1229a7e4ac5a39f3f44309
jb.diku <jb.diku@b75ad72c-e7d7-11dd-a971-7dbc132099af>
2010-04-20 15:16:29 +0000

    mime type detection and directory listing for private VGrid web space

--- branches/grid.dk/mig/shared/functionality/showvgridprivatefile.py
+++ branches/grid.dk/mig/shared/functionality/showvgridprivatefile.py
@@ -32,10 +32,12 @@ they can access them like owners do.
 """
 
 import os
+import time
 
 import shared.returnvalues as returnvalues
 from shared.functional import validate_input_and_cert, REJECT_UNSET
-from shared.init import initialize_main_variables
+from shared.init import initialize_main_variables, find_entry
+import shared.magic as magic
 from shared.validstring import valid_user_path
 from shared.vgrid import vgrid_is_owner_or_member
 
@@ -46,6 +48,172 @@ def signature():
     defaults = {'vgrid_name': REJECT_UNSET, 'path': REJECT_UNSET}
     return ['file_output', defaults]
 
+# helpers for directory listing
+def scale_bytes(bytes):
+
+    """Scale bytes to an appropriate size, returning the scaled number
+       and an appropriate suffix as one string for pretty printing"""
+
+    # factors/letters have to be descending.
+
+    factors = zip( [30,20,10], ['G','M','K'])
+
+    for (k,s) in factors:
+
+        x = bytes*1.0 / pow(2,k)
+        if x > 1:
+
+            return ( ("%.3f" % x) + ' ' + s+'B')
+
+    # if not successful, we reach here. Return Bytes.
+
+    return (str(bytes) + ' B')
+
+
+# javascript to include for sorting
+dir_js = '''
+<script type="text/javascript" src="/images/js/jquery-1.3.2.min.js"></script>
+<script type="text/javascript" src="/images/js/jquery.tablesorter.js"></script>
+
+<style type="text/css">
+table {
+  background: #eef;
+  color: #000;
+  margin-right: auto;
+  margin-left: auto;
+  text-align: left;
+  border: 1px solid #000;
+}
+// tablesorter...
+#filetable .header { 
+    background-image: url('/images/icons/small.gif'); 
+    cursor: pointer; 
+    font-weight: bold; 
+    background-repeat: no-repeat; 
+    background-position: center left; 
+    padding-left: 20px; 
+    border-right: 1px solid #dad9c7; 
+    margin-left: -1px; 		
+} 
+#filetable .headerSortDown{ 
+    background-image: url('/images/icons/asc.gif'); 
+    background-repeat: no-repeat; 
+    background-color: #3399FF; 
+}
+#filetable .headerSortUp { 
+    background-image: url('/images/icons/desc.gif'); 
+    background-repeat: no-repeat; 
+    background-color: #3399FF; 
+}
+.odd {
+  background-color: white;
+}
+.even {
+  background-color: #ccc;
+}
+.sortkey { display:none; }
+</style>
+
+<script type="text/javascript" >
+
+$(document).ready(function() {
+          var getsortkey = function(contents) {
+              var key = $(contents).find(".sortkey").text();
+              if (key == null) {
+                   key = $(contents).html();
+              }
+              return key;
+          }
+          $("#filetable").tablesorter({widgets: ["zebra"],
+                                        sortList: [[0,0],[1,0]],
+                                        textExtraction: getsortkey
+                                       });
+});
+</script>
+'''
+    
+def list_dir(filename, vgrid_name, rel_path):
+    """ generate html table with directory listing"""
+
+    if not os.path.isdir(filename):
+        return ('Internal error: %s not a directory' % filename)
+
+    html = '''
+  <div style="text-align:center">
+  <h3>%s: directory index of %s</h3>
+  </div>
+  <div style="text-align:center;margin-left:100px;margin-right:100px">
+    <table id="filetable">
+     <thead>
+      <th style="text-align:left"><!-- symbol --></th>
+      <th style="text-align:left">Name</th>
+      <th style="width:120px; text-align:right">Size</th>
+      <th style="width:200px; text-align:left">Last Modified</th>
+     </thead>
+     <tbody>
+''' % (vgrid_name,rel_path)
+
+    # always there: link to upper level
+    html +='''
+      <tr><td><img src="/images/icons/arrow_redo.png">
+          <td><a href="/vgrid/%s/path/%s">
+                 Up to higher level directory</a>
+          <td><td></tr><tr/>
+''' % (vgrid_name,os.path.dirname(rel_path.rstrip('/')) + '/')
+
+
+    # read and sort file names
+    contents = os.listdir(filename)
+    contents.sort()
+
+    for f in contents:
+        # for file in dir: get info, print it...
+        f_path = os.path.join(filename, f)
+        link = "/vgrid/%s/path/%s" % \
+               (vgrid_name, os.path.join(rel_path,f))
+        info = os.stat(f_path)
+        size = scale_bytes(info.st_size)
+        #size = str(info.st_size)
+        key  = 'file:' + f
+        date = time.strftime("%Y-%m-%d %H:%M:%S",
+                             time.localtime(info.st_mtime))
+        type = magic.file(f_path)
+        if os.path.isdir(f_path):
+            type = 'directory'
+            key  = 'dir:' + f
+            size = '-'
+
+        elif type.startswith('image/'):
+            type = 'picture'
+        elif type == 'text/html':
+            type = 'html'
+        elif type.startswith('text/') or type.startswith('message/'):
+            type = 'txt'
+        elif type.startswith('application/p'):
+            type = 'pdf'
+        elif type.endswith('zip') or type.endswith('zip2'):
+            type = 'compress'
+        elif type.startswith('application/octet'):
+            type = 'computer'
+        elif type.startswith('audio/') or type.startswith('video/'):
+            type = 'music'
+        else:
+            type = 'file'
+
+        html += '''
+      <tr><td><img src="/images/icons/%s.png">
+              <div class="sortkey">%s</div>
+          <td><div class="sortkey">%s</div><a href="%s">%s</a>
+      <td style="text-align:right"><div class="sortkey">%s</div>%s
+      <td><div class="sortkey">%s</div>%s</tr>
+''' % (type, key, f, link, f, size[-2:] + size, size, date, date)
+
+    html += '''
+     </tbody>
+    </table>
+</div>
+'''
+    return html
 
 def main(client_id, user_arguments_dict):
     """Main function used by front end"""
@@ -90,13 +258,35 @@ access the private files.''' % vgrid_name})
                                '''You are not allowed to use paths outside vgrid
 private files dir.'''})
         return (output_objects, returnvalues.CLIENT_ERROR)
+
+    try:
+        # analyse file contents using python-magic (own customised module)
+        mime_type = magic.file(real_path)
+    except Exception, exc:
+        # default to plain text
+        mime_type = 'text/plain'
     
+    if mime_type == 'directory':
+
+        # generate directory listing, include menu in page
+        title = find_entry(output_objects, 'title')
+        title['javascript'] = dir_js
+
+        output_objects.append({'object_type': 'html_form',
+                               'text': list_dir(real_path,vgrid_name,rel_path)
+                               })
+        return (output_objects, returnvalues.OK)
+
+    # otherwise, normal file: serve the raw file with suitable mime type
+
     try:
         private_fd = open(real_path, 'rb')
         entry = {'object_type': 'binary',
                  'data': private_fd.read()}
         # Cut away all the usual web page formatting to show only contents
-        output_objects = [{'object_type': 'start', 'headers': []}, entry,
+        output_objects = [{'object_type': 'start',
+                           'headers': [('Content-Type',mime_type)]},
+                          entry,
                           {'object_type': 'script_status'},
                           {'object_type': 'end'}]
         private_fd.close()

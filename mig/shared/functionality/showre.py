ff72313160eb5dd30956dd698740f11cc4b6b8b8
jesperrude <jesperrude@b75ad72c-e7d7-11dd-a971-7dbc132099af>
2010-04-29 13:11:26 +0000

    Added confirm dialogs, so that the users can confirm deletion of RTE's and resources.

--- mig/shared/functionality/showre.py
+++ mig/shared/functionality/showre.py
@@ -154,9 +154,104 @@ def main(client_id, user_arguments_dict):
     output_objects.append(build_reitem_object_from_re_dict(re_dict))
 
     if client_id ==  re_dict['CREATOR']:
-        output_objects.append({'object_type': 'link', 'destination': 'deletere.py?re_name=' + re_name,
-                           'class': 'removelink', 'title': 'Delete RTE',
-                           'text': 'Delete'})
+        
+        output_objects.append({'object_type': 'link',
+                                       'destination':
+                                       "javascript:runConfirmDialog('%s','%s');" % \
+                                       ("Really delete runtime environment " + re_name + "?", 
+                                        'deletere.py?re_name=%s'\
+                                        % (re_name)),
+                                       'class': 'removelink',
+                                       'title': 'Delete runtime environment %s' % re_name, 
+                                       'text': 'Delete'})
+
+
+    title_entry = find_entry(output_objects, 'title')
+    title_entry['text'] = 'Runtime environment details'
+
+    # jquery support for tablesorter and confirmation on "leave":
+
+    title_entry['javascript'] = '''
+<link rel="stylesheet" type="text/css" href="/images/css/jquery.managers.css" media="screen"/>
+<link rel="stylesheet" type="text/css" href="/images/css/jquery-ui-1.7.2.custom.css" media="screen"/>
+
+<script type="text/javascript" src="/images/js/jquery-1.3.2.min.js"></script>
+<script type="text/javascript" src="/images/js/jquery.tablesorter.js"></script>
+<script type="text/javascript" src="/images/js/jquery-ui-1.7.2.custom.min.js"></script>
+
+<script type="text/javascript" >
+
+var runConfirmDialog = function(text, link, textFieldName) {
+
+    if (link == undefined) {
+        link = "#";
+    }
+    if (text == undefined) {
+        text = "Are you sure?";
+    }
+    $( "#confirm_text").html(text);
+
+    var addField = function() { /* doing nothing... */ };
+    if (textFieldName != undefined) {
+        $("#confirm_input").show();
+        addField = function() {
+            link += textFieldName + "=" + $("#confirm_input")[0].value;
+        }
+    }
+
+    $( "#confirm_dialog").dialog("option", "buttons", {
+              "No": function() { $("#confirm_input").hide();
+                                 $("#confirm_text").html("");
+                                 $("#confirm_dialog").dialog("close");
+                               },
+              "Yes": function() { addField();
+                                  window.location = link;
+                                }
+            });
+    $( "#confirm_dialog").dialog("open");
+}
+
+$(document).ready(function() {
+
+          // init confirmation dialog
+          $( "#confirm_dialog" ).dialog(
+              // see http://jqueryui.com/docs/dialog/ for options
+              { autoOpen: false,
+                modal: true, closeOnEscape: true,
+                width: 500,
+                buttons: {
+                   "Cancel": function() { $( "#" + name ).dialog("close"); }
+	        }
+              });
+
+          // table initially sorted by col. 2 (admin), then 1 (member), then 0
+          var sortOrder = [[2,1],[1,1],[0,0]];
+
+          // use image path for sorting if there is any inside
+          var imgTitle = function(contents) {
+              var key = $(contents).find("a").attr("class");
+              if (key == null) {
+                  key = $(contents).html();
+              }
+              return key;
+          }
+
+          $("#vgridtable").tablesorter({widgets: ["zebra"],
+                                        sortList:sortOrder,
+                                        textExtraction: imgTitle
+                                       });
+     }
+);
+</script>
+'''
+
+    output_objects.append({'object_type': 'html_form',
+                           'text':'''
+ <div id="confirm_dialog" title="Confirm" style="background:#fff;">
+  <div id="confirm_text"><!-- filled by js --></div>
+   <textarea cols="40" rows="4" id="confirm_input" style="display:none;"/></textarea>
+ </div>
+'''                       })
 
     
     return (output_objects, returnvalues.OK) 

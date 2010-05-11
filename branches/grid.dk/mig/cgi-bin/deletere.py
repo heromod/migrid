30a431a1a2ad601d8cae7e41e1ff4c11a84e9cd2
jesperrude <jesperrude@b75ad72c-e7d7-11dd-a971-7dbc132099af>
2010-05-11 08:49:39 +0000

    Added comment and doc string, according to Jonas comment to revision 1063

--- branches/grid.dk/mig/cgi-bin/deletere.py
+++ branches/grid.dk/mig/cgi-bin/deletere.py
@@ -3,7 +3,7 @@
 #
 # --- BEGIN_HEADER ---
 #
-# showre - [insert a few words of module description on this line]
+# deletere - Deteles a runtimeenvironment
 # Copyright (C) 2003-2009  The MiG Project lead by Brian Vinter
 #
 # This file is part of MiG.

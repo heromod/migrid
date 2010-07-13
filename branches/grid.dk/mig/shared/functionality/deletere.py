78e49d2327985d281d134d2265ba84c2eb355f5a
jesperrude <jesperrude@b75ad72c-e7d7-11dd-a971-7dbc132099af>
2010-07-13 13:40:30 +0000

    Cleaned up comments and imports
    Added ownercheck so that non-owners can not delete RTE's by calling deletere.py?re_name=X

--- branches/grid.dk/mig/shared/functionality/deletere.py
+++ branches/grid.dk/mig/shared/functionality/deletere.py
@@ -4,8 +4,6 @@
 # --- BEGIN_HEADER ---
 #
 
-# deletere - Deletes a runtime environment
-
 # deletere - Deletes a runtimeenvironment
 # Copyright (C) 2003-2009  The MiG Project lead by Brian Vinter
 #
@@ -32,8 +30,6 @@
 """Deletion of  runtimeenvironments"""
 
 
-
-
 import time
 import base64
 import fcntl
@@ -42,8 +38,11 @@ import os
 import shared.returnvalues as returnvalues
 from shared.functional import validate_input_and_cert, REJECT_UNSET
 from shared.init import initialize_main_variables, find_entry
-from shared.refunctions import is_runtime_environment, get_re_dict, del_re, del_re_from_resources, is_re_active
-from shared.vgridaccess import user_visible_resources, get_resource_map,get_vgrid_map, OWNERS, CONF
+from shared.refunctions import is_runtime_environment, \
+     get_re_dict, del_re, is_re_active
+
+from shared.vgridaccess import user_visible_resources, \
+     get_resource_map,get_vgrid_map, OWNERS, CONF
 
 
 
@@ -78,10 +77,28 @@ def main(client_id, user_arguments_dict):
 
     re_name = accepted['re_name'][-1]
 
-
-    #In order to ensure referential integrity, the RTE re_name must be removed from
-    #all resources that has an entry for re_name: 
+    #Check whether re_name represents a runtimeenvironment.
+    if not is_runtime_environment(re_name, configuration):
+        output_objects.append({'object_type': 'error_text','text': \
+            'Runtime environment %s does not exists!'%re_name})
+        return (output_objects, returnvalues.SYSTEM_ERROR)
     
+
+    re_dict = get_re_dict(re_name, configuration)
+
+
+    #Verifying that the RTE belongs to the user, that tries to delete it.
+    if not re_dict[0]:
+        output_objects.append({'object_type': 'error_text','text': \
+        'Could not read runtime environment details for runtime environment %s'%re_name})
+        return (output_objects, returnvalues.SYSTEM_ERROR)
+
+    
+    if not client_id == re_dict[0]['CREATOR']:
+        output_objects.append({'object_type': 'error_text','text': \
+        'You are not the owner of runtime environment "%s"' %re_name})
+        return (output_objects, returnvalues.SYSTEM_ERROR)
+
     #getting a map of all resources
     res_map = get_resource_map(configuration)
 
